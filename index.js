88aac212fd9735efae3b6e30ca69a1fe43f7143e
Nyasia Richardson <rnyasia@yahoo.com>
2019-07-09 03:13:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,10 +10,4 @@ function logShout(string) {
 function logWhisper(string) {
   return console.log (string.toLowerCase());
 }
-function sayHiToGrandma(string){
-  if(string === string.toLowerCase()) return "I cant hear you!";
-
-else if (string === "I love you Grandma.") return "I love you, too";
-else if (string === string.toUpperCase()) return "YES INDEED";
   
-}
